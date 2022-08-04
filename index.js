0f05dab4156eac04d00d80f40e2fa1ff3a409e24
imubarak234 <imubarak2424@gmail.com>
2022-08-04 18:31:38 +0100

    token setup

--- index.js
+++ index.js
@@ -128,5 +128,6 @@ let unifiedServer = function(req, res){
 // Define a request router
 let router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens' : handlers.tokens,
 };
